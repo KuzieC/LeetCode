244fb0a974009cf89069ee1ad5174beba49f2a1d
Kuzie <kuziechen@outlook.com>
2025-01-14 14:51:31 +0000

    add solutions for LeetCode problems: Spiral Matrix, Find Peak Element, Rotate Image; include implementations and update bricks.json

---
+++ 48.Rotate Image.cpp
@@ -0,0 +1,114 @@
+/*
+ * @lc app=leetcode id=48 lang=cpp
+ * @lcpr version=20004
+ *
+ * [48] Rotate Image
+ *
+ * https://leetcode.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (76.75%)
+ * Likes:    18232
+ * Dislikes: 870
+ * Total Accepted:    2.1M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * You are given an n x n 2D matrix representing an image, rotate the image by
+ * 90 degrees (clockwise).
+ * 
+ * You have to rotate the image in-place, which means you have to modify the
+ * input 2D matrix directly. DO NOT allocate another 2D matrix and do the
+ * rotation.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // 先沿对角线反转二维矩阵
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // swap(matrix[i][j], matrix[j][i]);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // 然后反转二维矩阵的每一行
+        for (auto& row : matrix) {
+            ::reverse(row.begin(), row.end());
+        }
+    }
+
+    // 反转一维数组
+    void reverse(vector<int>& arr) {
+        int i = 0, j = arr.size() - 1;
+        while (j > i) {
+            // swap(arr[i], arr[j]);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[4,5,6],[7,8,9]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]\n
+// @lcpr case=end
+
+ */
+
---
+++ 54.Spiral Matrix.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=54 lang=cpp
+ * @lcpr version=20004
+ *
+ * [54] Spiral Matrix
+ *
+ * https://leetcode.com/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (52.37%)
+ * Likes:    15518
+ * Dislikes: 1394
+ * Total Accepted:    1.7M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[4,5,6],[7,8,9]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,2,3,4],[5,6,7,8],[9,10,11,12]]\n
+// @lcpr case=end
+
+ */
+
