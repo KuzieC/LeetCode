6829d6e1a593e9c20b14e56c10a2afcf3b47deb6
Kuzie <kuziechen@outlook.com>
2025-01-14 12:11:50 +0000

    add solution for LeetCode problem: Reverse Words in a String; include implementation and update bricks.json

---
+++ 151.Reverse Words in a String.cpp
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode id=151 lang=cpp
+ * @lcpr version=20004
+ *
+ * [151] Reverse Words in a String
+ *
+ * https://leetcode.com/problems/reverse-words-in-a-string/description/
+ *
+ * algorithms
+ * Medium (48.81%)
+ * Likes:    9024
+ * Dislikes: 5296
+ * Total Accepted:    2M
+ * Total Submissions: 4.2M
+ * Testcase Example:  '"the sky is blue"'
+ *
+ * Given an input string s, reverse the order of the words.
+ * 
+ * A word is defined as a sequence of non-space characters. The words in s will
+ * be separated by at least one space.
+ * 
+ * Return a string of the words in reverse order concatenated by a single
+ * space.
+ * 
+ * Note that s may contain leading or trailing spaces or multiple spaces
+ * between two words. The returned string should only have a single space
+ * separating the words. Do not include any extra spaces.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: s = "the sky is blue"
+ * Output: "blue is sky the"
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: s = "  hello world  "
+ * Output: "world hello"
+ * Explanation: Your reversed string should not contain leading or trailing
+ * spaces.
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: s = "a good   example"
+ * Output: "example good a"
+ * Explanation: You need to reduce multiple spaces between two words to a
+ * single space in the reversed string.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s contains English letters (upper-case and lower-case), digits, and spaces '
+ * '.
+ * There is at least one word in s.
+ * 
+ * 
+ * 
+ * Follow-up: If the string data type is mutable in your language, can you
+ * solve it in-place with O(1) extra space?
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <sstream>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        stringstream ss(s);
+        string word;
+        vector<string> v;
+        while(ss >> word){
+            reverse(word.begin(),word.end());
+            v.push_back(word);
+        }
+        string r;
+        for(auto i : v){
+            r+= i;
+            r+= " ";
+        }
+        r.pop_back();
+        return r;
+
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "the sky is blue"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "  hello world  "\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "a good   example"\n
+// @lcpr case=end
+
+ */
+
