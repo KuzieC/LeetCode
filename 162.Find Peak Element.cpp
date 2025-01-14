5e1a052d360b72dd6c00eb2fc31641961797c535
Kuzie <kuziechen@outlook.com>
2025-01-14 15:03:48 +0000

    add solution for LeetCode problem: Find Peak Element; improve implementation and update bricks.json

--- 162.Find Peak Element.cpp
+++ 162.Find Peak Element.cpp
@@ -75,7 +75,18 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        
+        int left = 0, right = nums.size() - 1;
+        while(left <= right){
+            int mid = left + (right-left)/2;;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.size() - 1 || nums[mid] > nums[mid + 1])) {
+                return mid;
+            } else if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 };
 // @lc code=end
