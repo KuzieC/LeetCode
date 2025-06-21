53a606cd73c451b4a807298993268cdb325f8b49
Kuzie <kuziechen@outlook.com>
2025-06-21 11:34:09 +0100

    Add solution for LeetCode problem: Two Sum

---
+++ leetcode/editor/en/two-sum.cpp
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ * @lcpr version=30201
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (55.78%)
+ * Likes:    62177
+ * Dislikes: 2254
+ * Total Accepted:    17.6M
+ * Total Submissions: 31.5M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ * 
+ * 
+ * 
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time
+ * complexity?
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); ++i){
+            if(mp.count(target - nums[i])){
+                res.push_back(mp[target-nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            else{
+                mp[nums[i]] = i;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// [2,7,11,15]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,3]\n6\n
+// @lcpr case=end
+
+ */
+
