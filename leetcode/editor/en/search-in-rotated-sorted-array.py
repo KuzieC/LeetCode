0bb8d33046ee6ddb48cfbe03603736dfb44cb087
Kuzie <kuziechen@outlook.com>
2025-06-30 15:02:11 +0100

    Add solution for LeetCode problem: Search in Rotated Sorted Array

---
+++ leetcode/editor/en/search-in-rotated-sorted-array.py
@@ -0,0 +1,100 @@
+#
+# @lc app=leetcode id=33 lang=python
+# @lcpr version=30201
+#
+# [33] Search in Rotated Sorted Array
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (42.90%)
+# Likes:    28189
+# Dislikes: 1709
+# Total Accepted:    3.7M
+# Total Submissions: 8.5M
+# Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+#
+# There is an integer array nums sorted in ascending order (with distinct
+# values).
+# 
+# Prior to being passed to your function, nums is possibly rotated at an
+# unknown pivot index k (1 <= k < nums.length) such that the resulting array is
+# [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
+# (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3
+# and become [4,5,6,7,0,1,2].
+# 
+# Given the array nums after the possible rotation and an integer target,
+# return the index of target if it is in nums, or -1 if it is not in nums.
+# 
+# You must write an algorithm with O(log n) runtime complexity.
+# 
+# 
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 5000
+# -10^4 <= nums[i] <= 10^4
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -10^4 <= target <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        lo, hi = 0,len(nums)-1
+        
+        while lo <= hi:
+            mid = lo + (hi - lo)/2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[lo]:
+                if target >= nums[lo] and nums[mid] > target:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        return -1
+                    
+                
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [4,5,6,7,0,1,2]\n0\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [4,5,6,7,0,1,2]\n3\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n0\n
+# @lcpr case=end
+
+#
+
